256b69877acfec37633fe42354233f10160d69fa
Ваше Имя <afifao1>
2025-03-19 16:00:16 +0500

    MAJOR:first working example

--- src/Controllers/Sleep.php
+++ src/Controllers/Sleep.php
@@ -3,8 +3,50 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
+date_default_timezone_set("Asia/Tashkent");
+
 class Sleep{
-  public function calculate(){
-    echo 'calculated';
-  }
+  public function agecalculator($birthday){
+    // var_dump($birthday);
+    $birthdayy = new \DateTime($birthday);
+    $today = new \DateTime();
+    $age = $today->diff($birthdayy)->y;
+    // var_dump($age);
+    $year_sleep = $this->time_of_age($age);
+    return $year_sleep;
+
+}
+
+  public function time_of_age($age){
+    $sleep = 0;//380+4745+13140+
+    if ($age >= 0) {
+      $sleep += 365 * 15; 
+    }
+    if ($age >= 1) {
+        $sleep += 365 * 13; 
+    }
+    if ($age >= 3) {
+        $sleep += 3 * 365 * 12; 
+    }
+    if ($age >= 6) {
+        $sleep += 8 * 365 * 10;
+    }
+    if ($age >= 14) {
+        $sleep += 4 * 365 * 9;
+    }
+    if ($age >= 18) {
+        $sleep += 47 * 365 * 8;
+    }
+    if ($age >= 65) {
+        $sleep += 10 * 365 * 7.5; 
+    }
+
+    // var_dump($sleep);
+
+     $day_sleep = (int)($sleep / 24);
+     $year_sleep = (int)($day_sleep / 365);
+    //  var_dump($year_sleep);
+    // return $day_sleep;
+    return $year_sleep;
+    } 
 }
--- views/sleep.php
+++ views/sleep.php
@@ -1,3 +1,30 @@
 <?php
+use \App\Controllers\Sleep;
+// (new \App\Controllers\Sleep())->calculate();
+?>
 
-(new \App\Controllers\Sleep())->calculate();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Sleep calculator</h1>
+    <form action="" method="POST">
+        <label for="birthday">Enter your year, month, and day of birth</label><br><br>
+        <input type="date" name="birthday"><br><br>
+        <button type="submit">Submit</button>
+        <?php
+        $birthday = $_POST["birthday"];
+        // var_dump($birthday);
+        $sleepObj = new Sleep();
+        $sleep=$sleepObj->agecalculator($birthday);
+        echo "<p>How many years did you sleep: " . $sleep . "</p>";
+        // var_dump($year_sleep);
+        // echo "<p>How many years did you sleep: " . $sleep['years'] . "</p>";
+        ?>
+    </form>
+</body>
+</html>
