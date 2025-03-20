183cf6391929d264066dff453ff969c487bc9477
Ваше Имя <afifao1>
2025-03-20 16:29:59 +0500

    PATCH: The code has been shortened and comments have been added.

--- src/Controllers/Sleep.php
+++ src/Controllers/Sleep.php
@@ -6,180 +6,89 @@ namespace App\Controllers;
 date_default_timezone_set("Asia/Tashkent");
 
 class Sleep{
-  public function agecalculator($birthday){
-    $year  = date("Y", strtotime($birthday)); 
-    $month = date("m", strtotime($birthday));  
-    $day   = date("d", strtotime($birthday));   
-    $guide = [[14, 17], [12, 16], [11, 14], [10, 13], [9, 12], [8, 10], [7, 9], [7, 8]];
-        $birth = new \DateTime("$year-$month-$day");
+    public function agecalculator($birthDay){
+        $year = date("Y", strtotime($birthDay));
+        $month = date("m", strtotime($birthDay));
+        $day = date("d", strtotime($birthDay));
+        $aldim = AVERAGE_LIFE_DURATION * 12; # o'rtacha kutiladigan hayot davomiyligi yildan oyga o'girildi.(AVERAGE LIFE DURATION IN MONTHS)
+        $davrlar = [15.5, 14, 12.5, 11.5, 10.5, 9, 8, 7.5];# Davrlar ketmaketligi oylarda: [0-3, 3-11, 22-24, 24-72, 72-156, 156-216, 216-768, 768-∞]
+        $daysInMonth = 365/12;
+        $birthDate = new \DateTime("$year-$month-$day");
         $now = new \DateTime();
-        $diff = $birth->diff($now);
+        $diff = $birthDate->diff($now);
         
-        $monthAge = ($diff->y * 12) + $diff->m + ($diff->d/30.4);
-        $monthAge = round($monthAge, 2);
-        // print($diff->d);
-        // print($monthAge);# 3.63
-        $avgHours = 0;
+        $livedMonths = ($diff->y * 12) + $diff->m + ($diff->d/$daysInMonth);
+        $livedMonths = round($livedMonths, 2);
+        $sleptHours = 0;
     
-        if ($monthAge <= 3){# 3
-            $avgHours = ($guide[0][0] + $guide[0][1]) / 2;
+        if ($livedMonths <= 3){
+            $sleptHours = $davrlar[0] * $livedMonths;
         } 
-        elseif ($monthAge <= 11){# 8
-            $avgHours = 
-            (
-                ((($guide[0][0] + $guide[0][1]) / 2) * 3) + 
-                (($guide[1][0] + $guide[1][1]) / 2) * ($monthAge - 3)
-            ) / (3 + ($monthAge - 3));
-            // print("Keldi");# ((15.5 * 3) + (14 * 0.63))/3.63 = (46.5 + 8.4)/3.63 = 54.9/3.63 = 15.23     1692
-        }
-        elseif ($diff->y < 2){# 13
-            $avgHours = (
-                ((($guide[0][0] + $guide[0][1]) / 2) * 3) + 
-                ((($guide[1][0] + $guide[1][1]) / 2) * 8) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * ($monthAge - 11))
-            ) / (3 + 8 + ($monthAge - 11));
-        }
-        elseif ($diff->y < 6){# 48
-            $avgHours = (
-                ((($guide[0][0] + $guide[0][1]) / 2) * 3) + 
-                ((($guide[1][0] + $guide[1][1]) / 2) * 8) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * 13) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * ($monthAge - 24))
-            ) / (3 + 8 + 13 + ($monthAge - 48));
-        }
-        elseif ($diff->y < 13){# 72
-            $avgHours = (
-                ((($guide[0][0] + $guide[0][1]) / 2) * 3) + 
-                ((($guide[1][0] + $guide[1][1]) / 2) * 8) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * 13) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * 48) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * ($monthAge - 72))
-            ) / (3 + 8 + 13 + 48 + ($monthAge - 72));
-        }
-        elseif ($diff->y < 19){# 72
-            $avgHours = (
-                ((($guide[0][0] + $guide[0][1]) / 2) * 3) + 
-                ((($guide[1][0] + $guide[1][1]) / 2) * 8) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * 13) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * 48) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * 84) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * ($monthAge - 156))
-            ) / (3 + 8 + 13 + 48 + 84 + ($monthAge - 156));
-        }
-        elseif ($diff->y < 65){# 552
-            $avgHours = (
-                ((($guide[0][0] + $guide[0][1]) / 2) * 3) + 
-                ((($guide[1][0] + $guide[1][1]) / 2) * 8) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * 13) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * 48) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * 84) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * 72) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * ($monthAge - 228))
-            ) / (3 + 8 + 13 + 48 + 84 + 72 + ($monthAge - 228));
-        }
-        else {
-            $avgHours = (
-                ((($guide[0][0] + $guide[0][1]) / 2) * 3) + 
-                ((($guide[1][0] + $guide[1][1]) / 2) * 8) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * 13) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * 48) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * 84) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * 72) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * 552) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ($monthAge - 780))
-            ) / (3 + 8 + 11 + 48 + 84 + 72 + 552 + ($monthAge - 780));
-        }
-    
-        // print($diff->days);# 111 * 15.23 = 1690.53
-        $totalHours = $diff->days * $avgHours;
-        $totalYears = ($totalHours / 24)/365;
-        // print($totalYears);
-        ;
-        $lifespanYears = AVERAGE_LIFE_DURATION;
-    
-        $deathYear = $year + $lifespanYears;
-        $death = new \DateTime("$deathYear-$month-$day");
-        $remainingDiff = $now->diff($death);
-        $remainingMonthAge = ($remainingDiff->y * 12) + $remainingDiff->m;
-        $remainingAvgHours = 0;
-    
-        if ($monthAge <= 3){# 3
-            $remainingAvgHours = (
-                ((($guide[0][0] + $guide[0][1]) / 2) * (3-$monthAge)) +
-                ((($guide[1][0] + $guide[1][1]) / 2) * 8) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * 13) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * 48) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * 84) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * 72) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * 552) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ((AVERAGE_LIFE_DURATION * 12)-780))
-            ) / ((3-$monthAge) + 8 + 11 + 48 + 84 + 72 + 552 + ((AVERAGE_LIFE_DURATION * 12)-780));
-        }
-        elseif ($monthAge <= 11){
-            $remainingAvgHours = (
-                ((($guide[1][0] + $guide[1][1]) / 2) * (11-$monthAge)) + 
-                ((($guide[2][0] + $guide[2][1]) / 2) * 13) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * 48) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * 84) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * 72) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * 552) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ((AVERAGE_LIFE_DURATION * 12)-780))
-            ) / ((11-$monthAge) + 11 + 48 + 84 + 72 + 552 + ((AVERAGE_LIFE_DURATION * 12)-780));
+        elseif ($livedMonths <= 11){
+            $sleptHours = ($davrlar[0] * 3) + ($davrlar[1] * ($livedMonths - 3));
         }
         elseif ($diff->y < 2){
-            $remainingAvgHours = (
-                ((($guide[2][0] + $guide[2][1]) / 2) * (24-$monthAge)) +
-                ((($guide[3][0] + $guide[3][1]) / 2) * 48) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * 84) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * 72) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * 552) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ((AVERAGE_LIFE_DURATION * 12)-780))
-            ) / ((24-$monthAge) + 48 + 84 + 72 + 552 + ((AVERAGE_LIFE_DURATION * 12)-780));
+            $sleptHours = ($davrlar[0] * 3) + ($davrlar[1] * 8) + ($davrlar[2] * ($livedMonths - 11));
         }
         elseif ($diff->y < 6){
-            $remainingAvgHours = (
-                ((($guide[3][0] + $guide[3][1]) / 2) * (72-$monthAge)) +
-                ((($guide[4][0] + $guide[4][1]) / 2) * 84) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * 72) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * 552) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ((AVERAGE_LIFE_DURATION * 12)-780))
-            ) / ((72-$monthAge)+ 84 + 72 + 552 + ((AVERAGE_LIFE_DURATION * 12)-780));
+            $sleptHours = ($davrlar[0] * 3) + ($davrlar[1] * 8) + ($davrlar[2] * 13) + ($davrlar[3] * ($livedMonths - 24));
         }
-        elseif ($remainingDiff->y < 13){
-            $remainingAvgHours = (
-                ((($guide[4][0] + $guide[4][1]) / 2) * (156-$monthAge)) +
-                ((($guide[5][0] + $guide[5][1]) / 2) * 72) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * 552) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ((AVERAGE_LIFE_DURATION * 12)-780))
-            ) / ((156-$monthAge) + 72 + 552 + ((AVERAGE_LIFE_DURATION * 12)-780));
+        elseif ($diff->y < 13){
+            $sleptHours = ($davrlar[0] * 3) + ($davrlar[1] * 8) + ($davrlar[2] * 13) + ($davrlar[3] * 48) +
+            ($davrlar[4] * ($livedMonths - 72));
         }
-        elseif ($remainingDiff->y < 19){
-            $remainingAvgHours = (
-                ((($guide[5][0] + $guide[5][1]) / 2) * (228-$monthAge)) +
-                ((($guide[6][0] + $guide[6][1]) / 2) * 552) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ((AVERAGE_LIFE_DURATION * 12)-780))
-            ) / ((228-$monthAge) + 552 + ((AVERAGE_LIFE_DURATION * 12)-780));
+        elseif ($diff->y < 19){
+            $sleptHours = ($davrlar[0] * 3) + ($davrlar[1] * 8) + ($davrlar[2] * 13) + ($davrlar[3] * 48) +
+            ($davrlar[4] * 84) + ($davrlar[5] * ($livedMonths - 156));
         }
-        elseif ($remainingDiff->y < 65){
-            $remainingAvgHours = (
-                ((($guide[6][0] + $guide[6][1]) / 2) * (780-$monthAge)) +
-                ((($guide[7][0] + $guide[7][1]) / 2) * ((AVERAGE_LIFE_DURATION * 12)-780))
-            ) / ((780-$monthAge) + ((AVERAGE_LIFE_DURATION * 12)-780));
+        elseif ($diff->y < 65){
+            $sleptHours = ($davrlar[0] * 3) + ($davrlar[1] * 8) + ($davrlar[2] * 13) + ($davrlar[3] * 48) +
+            ($davrlar[4] * 84) + ($davrlar[5] * 72) + ($davrlar[6] * ($livedMonths - 228));
         }
-        else{(($guide[7][0] + $guide[7][1]) / 2) * (((AVERAGE_LIFE_DURATION * 12)-$monthAge));}
+        else {
+            $sleptHours = ($davrlar[0] * 3) + ($davrlar[1] * 8) + ($davrlar[2] * 13) + ($davrlar[3] * 48) +
+            ($davrlar[4] * 84) + ($davrlar[5] * 72) + ($davrlar[6] * 552) + ($davrlar[7] * ($livedMonths - 780));
+        }
+    
+        $sleptHours *= $daysInMonth;
+        $sleptYears = ($sleptHours / 24)/365;
 
+        # Bu yerdan boshlab kelajakda qancha uxlashini hisoblash hisoblanadi.
+        ;
     
+        if ($livedMonths <= 3){
+            $remainingHours = ($davrlar[0] * (3 - $livedMonths)) + ($davrlar[1] * 8) + ($davrlar[2] * 13) + ($davrlar[3] * 48) +
+            ($davrlar[4] * 84) + ($davrlar[5] * 72) + ($davrlar[6] * 552) + ($davrlar[7] * ($aldim - 780));
+        }
+        elseif ($livedMonths <= 11){
+            $remainingHours = ($davrlar[1] * (11 - $livedMonths)) + ($davrlar[2] * 13) + ($davrlar[3] * 48) +
+            ($davrlar[4] * 84) + ($davrlar[5] * 72) + ($davrlar[6] * 552) + ($davrlar[7] * ($aldim - 780));
+        }
+        elseif ($diff->y < 2){
+            $remainingHours = ($davrlar[2] * (24 - $livedMonths)) + ($davrlar[3] * 48) + ($davrlar[4] * 84) +
+             ($davrlar[5] * 72) + ($davrlar[6] * 552) + ($davrlar[7] * ($aldim - 780));
+        }
+        elseif ($diff->y < 6){
+            $remainingHours = ($davrlar[3] * (72 - $livedMonths)) + ($davrlar[4] * 84) + ($davrlar[5] * 72) +
+            ($davrlar[6] * 552) + ($davrlar[7] * ($aldim - 780));
+        }
+        elseif ($diff->y < 13){
+            $remainingHours = ($davrlar[4] * (156 - $livedMonths)) + ($davrlar[5] * 72) + ($davrlar[6] * 552) +
+            ($davrlar[7] * ($aldim - 780));
+        }
+        elseif ($diff->y < 19){
+            $remainingHours = ($davrlar[5] * (228 - $livedMonths)) + ($davrlar[6] * 552) + ($davrlar[7] * ($aldim - 780));
+        }
+        elseif ($diff->y < 65){
+            $remainingHours = ($davrlar[6] * (780 - $livedMonths)) + ($davrlar[7] * ($aldim - 780));
+        }
+        else{
+            $remainingHours = $davrlar[7] * ($aldim - $livedMonths);
+        }
     
-    
-        $remainingTotalHours = $remainingDiff->days * $remainingAvgHours;
-        $remainingTotalYears = ($remainingTotalHours / 24)/365;
-        return ['hours' => round($totalHours), 'years' => round($totalYears, 2), 'remainingHours' => round($remainingTotalHours), 'remainingYears' => round($remainingTotalYears, 2), 'data' => $guide];
+        $remainingHours *= $daysInMonth;
+        $remainingYears = ($remainingHours / 24)/365;
+        return ['hours' => round($sleptHours), 'years' => round($sleptYears, 2), 'remainingHours' => round($remainingHours), 'remainingYears' => round($remainingYears, 2)];
     }
 }
-
 ?>
-
-
-
-
- 
