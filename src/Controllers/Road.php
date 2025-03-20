157aac891be5d1403c6c56abe926499dbc843356
AkmalovAbduvoris <abduvorisakmalov5005@gmail.com>
2025-03-20 17:13:50 +0500

    PATCH: Refactor Road class by removing commented-out code and improving travelTime method

--- src/Controllers/Road.php
+++ src/Controllers/Road.php
@@ -3,46 +3,6 @@ declare(strict_types=1);
 
 namespace App\Controllers;
 
-// class Road {
-
-//   public function roadHours(string $age) {
-    
-//   }
-
-//   public function travelTime($age) {
-//     if (!is_numeric($age)) {
-//       return "Invalid input";
-//     }
-//     $age = (int) $age;
-//     $travelHours = [
-//       '18-24' => 2,
-//       '25-54' => 2.5,
-//       '55-64' => 1.5,
-//       '65+'   => 1
-//     ];
-
-//     $totalTravelHours = 0;
-//       foreach ($travelHours as $range => $hoursPerDay) {
-//         [$min, $max] = explode('-', $range . '-');
-//         $min = (int)$min;
-//         $max = $max ? (int)$max : PHP_INT_MAX;
-
-//         if ($age > $min) {
-//           $years = min($age, $max) - $min;
-//           $totalTravelHours += 365 * $hoursPerDay * $years;
-//         }
-//       }
-
-    
-//       $avgDrivingSpan = 365 * (2 * 6 + 2.5 * 30 + 1.5 * 10 + 1 * 11);
-//       $leftTravelHours = $avgDrivingSpan - $totalTravelHours;
-//       return [
-//         "totalTravelTime" => $totalTravelHours,
-//         "avgTravel" => $avgDrivingSpan,
-//         "leftTravel" => $leftTravelHours
-//       ];
-//   }
-// }
 class Road {
   public function travelTime($age) {
     if (!is_numeric($age)) {
