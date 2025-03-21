d9fab11738cbdc8ce0da0c4eff1ce305142d0fe9
afifao1 <afifao1>
2025-03-21 21:17:33 +0500

    PATCH: The slept section now looks the same as everyone else's.

--- src/Controllers/Study.php
+++ src/Controllers/Study.php
@@ -1,34 +1,34 @@
-<!-- <?php
-    declare(strict_types=1);
+<?php
+declare(strict_types=1);
 
-    namespace Controllers;
+namespace Controllers;
 
-    class Study implements \Interfaces\Interfaces {
+class Study implements \Interfaces\Interfaces {
 
-        public function calculateHours($age, $hoursByPeriods, $annualSpent) {
-            
-            $totalHours = 0;
+    public function calculateHours($age, $hoursByPeriods, $annualSpent) {
+        
+        $totalHours = 0;
 
-            if ($age >= 18) {
-                foreach ($hoursByPeriods as $range => $hoursPerDay) {
+        if ($age >= 18) {
+            foreach ($hoursByPeriods as $range => $hoursPerDay) {
 
-                    [$periodStart, $periodEnd] = explode('-', $range . '-');
-                    
-                    if ($age > $periodStart) {
-                        $years = min($age, $periodEnd) - $periodStart;
-                        $totalHours += $annualSpent * $years*$hoursPerDay;
-                    }
+                [$periodStart, $periodEnd] = explode('-', $range . '-');
+                
+                if ($age > $periodStart) {
+                    $years = min($age, $periodEnd) - $periodStart;
+                    $totalHours += $annualSpent * $years*$hoursPerDay;
                 }
             }
-            
-            $avgWorkSpan = $annualSpent * (4 * 6 + 8 * 29 + 7 * 9 + 5 * 10);
-            $leftWorkHours = $avgWorkSpan - $totalHours;
-            
-            return [
-                "worked" => $totalHours,
-                "leftWork" => $leftWorkHours,
-                "avgTotal" => $avgWorkSpan
-            ];
         }
+        
+        $avgWorkSpan = $annualSpent * (4 * 6 + 8 * 29 + 7 * 9 + 5 * 10);
+        $leftWorkHours = $avgWorkSpan - $totalHours;
+        
+        return [
+            "worked" => $totalHours,
+            "leftWork" => $leftWorkHours,
+            "avgTotal" => $avgWorkSpan
+        ];
     }
-?> -->
+}
+?>
--- views/sleep.php
+++ views/sleep.php
@@ -1,13 +1,8 @@
-<h2>Sleep</h2>
-
 <?php
 use \App\Controllers\Sleep;
 $sleepObj = (new Sleep())->agecalculator($birthday);
-echo "Born $birthday, slept ~ {$sleepObj['hours']} hours or {$sleepObj['years']} years.<br> <br>";
-echo "Remaining sleep ~ {$sleepObj['remainingHours']} hours or {$sleepObj['remainingYears']} years.<br><br>";
-echo "Common sleeping bear ~ {$sleepObj['totalSleepHours']} hours or {$sleepObj['totalSleepYears']} years.<br><br>"
 ?>
-
-
-
-
+<h2>Slept</h2>
+<label>Total Slept: <input type="text" value=" <?= $sleepObj['hours'] ?> hours or <?= $sleepObj['years'] ?> years." readonly></label><br><br>
+<label>Remaining Sleep: <input type="text" value=" <?= $sleepObj['remainingHours'] ?> hours or <?= $sleepObj['remainingYears'] ?> years." readonly></label><br><br>
+<label>Common Sleeping Bear: <input type="text" value=" <?= $sleepObj['totalSleepHours'] ?> hours or <?= $sleepObj['totalSleepYears'] ?> years." readonly></label><br><br>
