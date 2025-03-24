2d66d489fbe1b893abcb8c165c17f1706e6d78b0
afifao1 <afifao1>
2025-03-24 17:06:48 +0500

    MINOR: Calculation class added

--- views/sleep.php
+++ views/sleep.php
@@ -1,8 +1,13 @@
 <?php
-use \App\Controllers\Sleep;
-$sleepObj = (new Sleep())->agecalculator($birthday);
+$sleepObj = new \App\Controllers\Sleep();   
+$person = new \App\Person($birthday);
+$interval = $person->period;
+$result=$sleepObj->sleepstat($interval);
+
+// use \App\Controllers\Sleep;
+// $sleepObj = (new Sleep())->agecalculator($birthday);
 ?>
 <h2>Slept</h2>
-<label>Total Slept: <input type="text" value=" <?= $sleepObj['hours'] ?> hours or <?= $sleepObj['years'] ?> years." readonly></label><br><br>
-<label>Remaining Sleep: <input type="text" value=" <?= $sleepObj['remainingHours'] ?> hours or <?= $sleepObj['remainingYears'] ?> years." readonly></label><br><br>
-<label>Common Sleeping Bear: <input type="text" value=" <?= $sleepObj['totalSleepHours'] ?> hours or <?= $sleepObj['totalSleepYears'] ?> years." readonly></label><br><br>
+<label>Total Slept: <input type="text" value=" <?= $result['hours'] ?> hours or <?= $result['years'] ?> years." readonly></label><br><br>
+<label>Remaining Sleep: <input type="text" value=" <?= $result['remainingHours'] ?> hours or <?= $result['remainingYears'] ?> years." readonly></label><br><br>
+<label>Common Sleeping Bear: <input type="text" value=" <?= $result['totalSleepHours'] ?> hours or <?= $result['totalSleepYears'] ?> years." readonly></label><br><br>
