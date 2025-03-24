ddd5424e269a1a23cad3a9a58cd7cb9a63f5cdb7
afifao1 <afifao1>
2025-03-25 03:01:24 +0500

    PATCH: refactoring sleep

--- views/sleep.php
+++ views/sleep.php
@@ -4,7 +4,7 @@ $person   = new \App\Person($birthday);
 $interval = $person->period;
 $result   = $sleepObj->sleepstat($interval);
 ?>
-<h2>Slept</h2>
+<h2>Sleep</h2>
 <label>Total Slept: <input type="text" value=" <?= $result['hours'] ?> hours or <?= $result['years'] ?> years." readonly></label><br><br>
 <label>Remaining Sleep: <input type="text" value=" <?= $result['remainingHours'] ?> hours or <?= $result['remainingYears'] ?> years." readonly></label><br><br>
-<label>Common Sleeping Bear: <input type="text" value=" <?= $result['totalSleepHours'] ?> hours or <?= $result['totalSleepYears'] ?> years." readonly></label><br><br>
+<label>Common Sleeping: <input type="text" value=" <?= $result['totalSleepHours'] ?> hours or <?= $result['totalSleepYears'] ?> years." readonly></label><br><br>
